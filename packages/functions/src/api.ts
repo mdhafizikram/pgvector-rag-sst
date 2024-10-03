45418189f502488b324b7d19e1ce638a55263c81
Arpita59 <arpita.kuntoje@oneorigin.us>
2024-10-04 01:00:57 +0530

    IAMDEV-89:Prompt fine tuning for user query validation

--- packages/functions/src/api.ts
+++ packages/functions/src/api.ts
@@ -237,18 +237,19 @@ async function generateEmbeddingOpenAI(text: string) {
 async function promptValidation(userQuery = "") {
   try {
     const validationPrompt = `
-      ### Role ###
-      AI Validator responsible for checking if the user query meets content guidelines.
-
-      ### Instructions ###
-      - The User Query should be focused on the student's interest to take up course. 
-      - The User Query should be  as per student's perspective . Possible query's student might ask.
-      - The User Query must not contain destructive content (e.g., harmful, violent, illegal, offensive, etc.).
-      - The User Query should not be a lame or non-sensible question.
-    
-      - If the User Query meets these conditions, return "true".
-      - If the User Query violates any of the conditions, return "false".
-
+       ### Role ###
+        AI Validator responsible for checking if the user query meets content guidelines.
+  
+        ### Instructions ###
+        - The User Query should be focused on the student's interest to take up course. 
+        - The User Query should be  as per student's perspective . Possible query's student might ask. 
+        - If User Query is fetching for  a course , the course field mentioned  should be related to student academic point or degree 
+        - The User Query must not contain destructive content (e.g., harmful, violent, illegal, offensive, etc.).
+        - The User Query should not be a lame or non-sensible question.
+        - The User Query might  include  just a perticular field name or qualification or a  short description about user's interest . 
+        - If the User Query meets these conditions, return "true".
+        - If the User Query violates any of the conditions, return "false".
+        
       ### User Query: ###
       ${userQuery}
     `;
